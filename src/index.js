2de0edfa8012cb10583255b1963bc07649cce8aa
RamezCh <ramiz_chreide@outlook.com>
2024-07-14 00:23:00 +0300

    Styling & Editing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
 // Doesn't have to be called App, it just needs to start with an Uppercase letter
 function App() {
   // Never nest components inside a div, always use a fragment
-  return <h1>Hello React!</h1>;
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
 }
 
 // React Components are just functions that return JSX
+// JSX is a syntax extension for JavaScript
+// It describes what the UI should look like and how it should behave
+// Basically HTML, CSS and JS combined
+// Note: React can be used without JSX, but it's not recommended
 // Function name needs to start with an Uppercase letter
 // Function needs to return markup like JSX or null or html elements
-function Pizza() {
-  return <h2>Pizza</h2>;
+function Manqoosha() {
+  return (
+    <div className="manqoosha">
+      <img src="images/Cheese Manakeesh.jpeg" alt="Cheese Manqoosha" />
+      <h3>Cheese Manqoosha</h3>
+      <p>Bread with olive oil topped with cheese</p>
+    </div>
+  );
+}
+
+function Header() {
+  // {} for JS mode, {} for object
+  // attribute: 'value'
+  return (
+    <header>
+      <h1>Yummy Triangles</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our menu</h2>
+      <Manqoosha />
+      <Manqoosha />
+      <Manqoosha />
+      <Manqoosha />
+    </main>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 6;
+  const closeHour = 22;
+  const isOpen = hour > openHour && hour < closeHour;
+  return (
+    <footer>
+      {new Date().toLocaleTimeString()}.
+      {isOpen ? `We're currently open` : `We're currently closed`}
+    </footer>
+  );
 }
 
 // React v18
--- src/manakeesh.js
+++ src/manakeesh.js
@@ -1,43 +1,48 @@
-// Manakeesh Data
 const manakeeshData = [
   {
     name: 'Cheese Manqoosha',
-    ingredients: 'Bread with italian olive oil and rosemary',
+    ingredients:
+      'Flatbread brushed with olive oil and topped with melted Akkawi cheese',
     price: 2,
     photoName: 'images/Cheese Manakeesh.jpeg',
     soldOut: false,
   },
   {
     name: 'Zaatar Manqoosha',
-    ingredients: 'Tomato and mozarella',
+    ingredients:
+      'Flatbread brushed with olive oil and topped with a blend of thyme, sumac, sesame seeds, and sea salt',
     price: 0.75,
     photoName: 'images/Zaatar-Manakeesh.jpg',
     soldOut: false,
   },
   {
     name: 'Half-Half Manqoosha',
-    ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
+    ingredients:
+      'Flatbread brushed with olive oil and topped half with melted Akkawi cheese and half with zaatar mix',
     price: 1.375,
     photoName: 'images/half-half manakeesh.jpg',
     soldOut: false,
   },
   {
     name: 'Meat Manqoosha',
-    ingredients: 'Tomato, mozarella, mushrooms, and onion',
+    ingredients:
+      'Flatbread topped with seasoned ground beef, diced tomatoes, onions, and a mix of Middle Eastern spices',
     price: 4,
     photoName: 'images/Meat Manakeesh.jpg',
     soldOut: false,
   },
   {
     name: 'Tomato & Onion Manqoosha',
-    ingredients: 'Tomato, mozarella, and pepperoni',
+    ingredients:
+      'Flatbread brushed with olive oil and topped with diced tomatoes, onions, and a mix of spices',
     price: 2,
     photoName: 'images/Tomato-and-Onion.jpg',
     soldOut: true,
   },
   {
     name: 'Spinach Fatayr',
-    ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
+    ingredients:
+      'Pastry filled with a mixture of spinach, onions, sumac, olive oil, and a hint of lemon juice',
     price: 3,
     photoName: 'images/spinach fatayr.jpg',
     soldOut: false,
