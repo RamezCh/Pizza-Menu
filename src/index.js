ce5d57b798dcd648aabfc74ae01ecbb3a7b2f7c3
RamezCh <ramiz_chreide@outlook.com>
2024-07-13 21:19:07 +0300

    Basic React

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+
+// Doesn't have to be called App, it just needs to start with an Uppercase letter
+function App() {
+  return <h1>Hello React!</h1>;
+}
+
+// React v18
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// StrictMode renders components twice to check for side effects
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// React before 18
+/*
+React.render(<App />);
+And don't use ReactDOM/client
+*/
