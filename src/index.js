d760b69d33b2414e04ae68209d8f4760c3e81de9
RamezCh <ramiz_chreide@outlook.com>
2024-07-13 22:30:26 +0300

    Change files and idea

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom/client';
 
 // Doesn't have to be called App, it just needs to start with an Uppercase letter
 function App() {
+  // Never nest components inside a div, always use a fragment
   return <h1>Hello React!</h1>;
+}
+
+// React Components are just functions that return JSX
+// Function name needs to start with an Uppercase letter
+// Function needs to return markup like JSX or null or html elements
+function Pizza() {
+  return <h2>Pizza</h2>;
 }
 
 // React v18
@@ -17,6 +25,6 @@ root.render(
 
 // React before 18
 /*
-React.render(<App />);
+React.render(<App />, document.getElementById('root'));
 And don't use ReactDOM/client
 */
---
+++ src/manakeesh.js
@@ -0,0 +1,45 @@
+// Manakeesh Data
+const manakeeshData = [
+  {
+    name: 'Cheese Manqoosha',
+    ingredients: 'Bread with italian olive oil and rosemary',
+    price: 2,
+    photoName: 'images/Cheese Manakeesh.jpeg',
+    soldOut: false,
+  },
+  {
+    name: 'Zaatar Manqoosha',
+    ingredients: 'Tomato and mozarella',
+    price: 0.75,
+    photoName: 'images/Zaatar-Manakeesh.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Half-Half Manqoosha',
+    ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
+    price: 1.375,
+    photoName: 'images/half-half manakeesh.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Meat Manqoosha',
+    ingredients: 'Tomato, mozarella, mushrooms, and onion',
+    price: 4,
+    photoName: 'images/Meat Manakeesh.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Tomato & Onion Manqoosha',
+    ingredients: 'Tomato, mozarella, and pepperoni',
+    price: 2,
+    photoName: 'images/Tomato-and-Onion.jpg',
+    soldOut: true,
+  },
+  {
+    name: 'Spinach Fatayr',
+    ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
+    price: 3,
+    photoName: 'images/spinach fatayr.jpg',
+    soldOut: false,
+  },
+];
